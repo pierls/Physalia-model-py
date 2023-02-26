34766913ef43519b27cf684449a4c955e2df2b73
pierls <80945072+pierls@users.noreply.github.com>
2023-02-26 23:21:02 +0100

    testing front features

--- app/app.py
+++ app/app.py
@@ -1,5 +1,14 @@
 from front import Front
+from model import compute
+
 
 def __init__():
     print("hello world")
-    app = Front()
+    app = Front().render()
+    
+    app.show_viewport()
+    app.start_dearpygui()
+    app.destroy_context()
+
+
+__init__()
--- app/front.py
+++ app/front.py
@@ -10,20 +10,30 @@ class Front(object):
 
     def render(self):
         dpg.create_context()
-        dpg.create_viewport()
+        dpg.create_viewport(title='Modelisation Population Physalia interactif', width=600, height=600)
         dpg.setup_dearpygui()
 
-        with dpg.window(label="Example Window"):
-            dpg.add_text("Hello world")
-            dpg.add_button(label="Save")
-            dpg.add_input_text(label="string")
-            dpg.add_slider_float(label="float")
 
-        dpg.show_viewport()
-        dpg.start_dearpygui()
-        dpg.destroy_context()
+        with dpg.window(label="Primary Window"):
+            with dpg.table(header_row=False):
+
+                # use add_table_column to add columns to the table,
+                # table columns use child slot 0
+                dpg.add_table_column()
+                dpg.add_table_column()
+
+        # add_table_next_column will jump to the next row
+        # once it reaches the end of the columns
+        # table next column use slot 1
+                for i in range(0, 2):
+                    with dpg.table_row():
+                        for j in range(0, 1):
+                            dpg.add_text(f"Row{i} Column{j}")
+
+        
+        #dpg.set_primary_window("Primary Window", True)
+            
 
 
+        return dpg
 
-temp = Front()
-temp.render()
