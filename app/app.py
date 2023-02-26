6900685107ca5a6d4f249a231873c94631ad8f16
pierls <80945072+pierls@users.noreply.github.com>
2023-02-27 00:22:16 +0100

    added some slides

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from front import Front
-from model import compute
+
 
 
 def __init__():
     print("hello world")
     app = Front().render()
-    
+
     app.show_viewport()
     app.start_dearpygui()
     app.destroy_context()
--- app/front.py
+++ app/front.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+from model import compute
 
 
 
@@ -6,10 +7,26 @@ import dearpygui.dearpygui as dpg
 
 
 class Front(object):
-
+    def __init__(self):
+        self.values = {}
+        self.values["a"]     = 0
+        self.values["b"]      = 0
+        self.values["Sp"]     = 0
+        self.values["d"]      = 0
+        self.values["Q"]      = 0
+        self.values["Sm"]     = 0
+        self.values["F"]      = 0
+        self.values["α"]      = 0
+        self.values["PO"]     = 0
 
     def render(self):
         dpg.create_context()
+        def print_value(sender):
+            a = dpg.get_value(sender)
+            label = dpg.get_item_label(sender)
+            print(label + " : "+str(a))
+            self.values["label"] = a
+
         dpg.create_viewport(title='Modelisation Population Physalia interactif', width=600, height=600)
         dpg.setup_dearpygui()
 
@@ -20,15 +37,37 @@ class Front(object):
                 # use add_table_column to add columns to the table,
                 # table columns use child slot 0
                 dpg.add_table_column()
-                dpg.add_table_column()
+                
 
-        # add_table_next_column will jump to the next row
-        # once it reaches the end of the columns
-        # table next column use slot 1
-                for i in range(0, 2):
-                    with dpg.table_row():
-                        for j in range(0, 1):
-                            dpg.add_text(f"Row{i} Column{j}")
+                with dpg.table_row():
+                    #function display here
+                    dpg.add_text(f"Function should be \ndisplayed here")
+                
+                with dpg.table_row():
+                    dpg.add_slider_float(label="a",default_value=0,callback=print_value, max_value=1)
+
+                with dpg.table_row():
+                    dpg.add_slider_float(label="b",default_value=0,callback=print_value, max_value=1)
+
+                with dpg.table_row():
+                    dpg.add_slider_float(label="Sp",default_value=0,callback=print_value, max_value=1)
+                
+                with dpg.table_row():
+                    dpg.add_slider_float(label="d",default_value=0,callback=print_value, max_value=1)
+                
+                with dpg.table_row():
+                    dpg.add_slider_float(label="Sm",default_value=0,callback=print_value, max_value=1)
+                
+                with dpg.table_row():
+                    dpg.add_slider_float(label="F",default_value=0,callback=print_value, max_value=1)
+                
+                with dpg.table_row():
+                    dpg.add_slider_float(label="α",default_value=0,callback=print_value, max_value=1)
+                
+                
+
+
+                column_2 = dpg.add_table_column()
 
         
         #dpg.set_primary_window("Primary Window", True)
