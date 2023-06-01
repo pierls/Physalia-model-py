8cef09ec776c6b874be3cc97146ae13f91fab0ef
logyrave <126420815+logyrave@users.noreply.github.com>
2023-06-01 21:35:14 +0200

    update commentary + sliders default_values. Last commit b4 LYON !!!

--- app/front.py
+++ app/front.py
@@ -35,52 +35,53 @@ class Front(object):
         - dearpygui (can be downloaded via ``pip install dearpygui``.)
         - model (internal class of this application.)
     """ 
+    # Création du dictionnaire de valeur, clés valeurs sous forme de tableau pour 
     def __init__(self):                     
         self.values = {}
-        self.values["Taux de rencontre méduse / prédateur"]             = 0
-        self.values["Taux de formation des supra-organismes"]           = 0
-        self.values["Taux de survie polype"]                            = 0
-        self.values["Taux de scissiparité par polype"]                  = 0
-        self.values["Quantité de prédateur initial"]                    = 0
-        self.values["Taux de survie par méduse"]                        = 0
-        self.values["Taux de fécondation par méduse"]                   = 0
-        self.values["Taux d'impact compétition intra-spécifique"]       = 0
-        self.values["Quantité de polype initial"]                       = 0
-        self.values["Période prévisionelle"]                            = 1
+        self.values["Taux de rencontre méduse / prédateur"]             = 0.5
+        self.values["Taux de formation des supra-organismes"]           = 0.02
+        self.values["Taux de survie polype"]                            = 0.7
+        self.values["Taux de scissiparité par polype"]                  = 0.4
+        self.values["Quantité de prédateur initial"]                    = 5
+        self.values["Taux de survie par méduse"]                        = 0.55
+        self.values["Taux de fécondation par méduse"]                   = 0.6
+        self.values["Taux d'impact compétition intra-spécifique"]       = 0.01
+        self.values["Quantité de polype initial"]                       = 10
+        self.values["Période prévisionelle (années)"]                   = 40
         self.values["results"]                                          = [0]
 
-# Création du plot 
+# Création du plot la boucle d'itération permet d'ajouter +1 aux abscisse pour chaque itération de modèle 
     def plot(self):
         """Function that act like a factory for the plot object of the UI.
         """
         with dpg.plot(label="", height=400, width=400):
             valeurs = []
-            for i in range(self.values["Période prévisionelle"]):
+            for i in range(self.values["Période prévisionelle (années)"]):
                 valeurs.append(i)
             # optionally create legend
             dpg.add_plot_legend()
 
             # REQUIRED: create x and y axes
-            dpg.add_plot_axis(dpg.mvXAxis, label="Période prévisionelle", tag="x_axis")
+            dpg.add_plot_axis(dpg.mvXAxis, label="Période prévisionelle (années)", tag="x_axis")
             dpg.add_plot_axis(dpg.mvYAxis, label="population", tag="y_axis")
 
             # series belong to a y axis
             dpg.add_line_series(valeurs, self.values["results"], label="Estimation de la population", parent="y_axis",tag="series")
 
-# Actualisation axe y et x respectivement en fonction du nombre d'individus calculés et de la période prévisionelle 
+# Actualisation axe y et x respectivement en fonction du nombre d'individus calculés et de la période prévisionelle grâce a Numpy (trouve la valeur max d'un tableau de valeur)
     def update_plot(self):
         """This function updates the previously created plot comùponent of the UI, based on
         the new parameters that the user have chosen. It calls the model's ``compute()`` function in order to display proper data.
         """
         annees = [0]
-        for i in range(self.values["Période prévisionelle"]):
+        for i in range(self.values["Période prévisionelle (années)"]):
             annees.append(i+1)
 
         print(str(self.values))
         print("results : " +str(self.values["results"]))
         dpg.set_value('series', [annees, self.values["results"]])
         dpg.set_axis_limits(axis = "y_axis",ymax=max(self.values["results"])+10, ymin=0)
-        dpg.set_axis_limits(axis = "x_axis", ymax=self.values["Période prévisionelle"], ymin = 0)
+        dpg.set_axis_limits(axis = "x_axis", ymax=self.values["Période prévisionelle (années)"], ymin = 0)
 
     def render(self):
         """Contrary to what its name might suggest, the ``render()`` function does not display anything, it only creates a viewport (in the DearPygui's way).
@@ -108,29 +109,29 @@ class Front(object):
                                              Sm=    self.values["Taux de survie par méduse"],
                                              f=     self.values["Taux de fécondation par méduse"],
                                              α=     self.values["Taux d'impact compétition intra-spécifique"],
-                                             years= self.values["Période prévisionelle"]
+                                             years= self.values["Période prévisionelle (années)"]
                                              )
             self.update_plot()
 
 
-# Création de l'interface
+# Création de l'interface sliders/input
 
         with dpg.window(tag="Primary Window"):          
             with dpg.table(header_row=False):
              dpg.add_table_column()
              with dpg.table_row():
                 with dpg.table_cell(tag="Sliders"):
-                    dpg.add_text(f"Function should be \ndisplayed here")
-                    dpg.add_slider_float(label="Taux de rencontre méduse / prédateur",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux de formation des supra-organismes",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux de survie polype",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux de scissiparité par polype",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux de survie par méduse",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux de fécondation par méduse",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_slider_float(label="Taux d'impact compétition intra-spécifique",default_value=0,callback=print_value, max_value=1)
-                    dpg.add_input_int(label="Quantité de prédateur initial",callback=print_value)
-                    dpg.add_input_int(label="Quantité de polype initial",callback=print_value)
-                    dpg.add_input_int(label="Période prévisionelle",callback=print_value)
+                    dpg.add_text("Modèle sous forme d'équation différentiel non linéaire")
+                    dpg.add_slider_float(label="Taux de rencontre méduse / prédateur",default_value=self.values["Taux de rencontre méduse / prédateur"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux de formation des supra-organismes",default_value=self.values["Taux de formation des supra-organismes"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux de survie polype",default_value=self.values["Taux de survie polype"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux de scissiparité par polype",default_value=self.values["Taux de scissiparité par polype"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux de survie par méduse",default_value=self.values["Taux de survie par méduse"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux de fécondation par méduse",default_value=self.values["Taux de fécondation par méduse"],callback=print_value, max_value=1)
+                    dpg.add_slider_float(label="Taux d'impact compétition intra-spécifique",default_value=self.values["Taux d'impact compétition intra-spécifique"],callback=print_value, max_value=1)
+                    dpg.add_input_int(label="Quantité de prédateur initial",default_value=self.values["Quantité de prédateur initial"],callback=print_value)
+                    dpg.add_input_int(label="Quantité de polype initial",default_value=self.values["Quantité de polype initial"],callback=print_value)
+                    dpg.add_input_int(label="Période prévisionelle (années)",default_value=self.values["Période prévisionelle (années)"],callback=print_value)
                     self.plot()
 
                
